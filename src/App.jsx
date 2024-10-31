b5abb9a1ad71808ab923be929d2bff76bfcfc042
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2024-10-31 12:18:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Webpack 5 with React 
+         Webpack 5 with React
         </p>
       </header>
     </div>
